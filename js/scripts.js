c6a52f9b5ea1917799833711913a0b6578a054e0
Kaili Nishihira and Robert Murray <kaili@>
2017-07-20 09:08:16 -0700

    Add div id coordinates

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+/*
+function Player(name, markType) {
+  this.name = name;
+  this.mark = markType;
+}
+
+function Space(coordinates, currentMark) {
+  this.coordinates = coordinates;
+  this.status = currentMark;
+}
+
+function Board(spaceArray) {
+  for (var i = 0; i < spaceArray.length; i++) {
+    this.spaces[i] = new Space(spaceArray[i], "");
+  }
+}
+
+function Game() {
+  this.board = new Board([[1,1],[1,2],[1,3],[2,1],[2,2],[2,3],[3,1],[3,2],[3,3]]);
+  this.player1 = new Player(1, X);
+  this.player2 = new Player(2, O);
+  this.turn = true;
+  this.gameOver = false;
+}
+*/
